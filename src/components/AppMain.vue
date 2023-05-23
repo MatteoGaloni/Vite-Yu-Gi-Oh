39221154b4c19e58b334f180547ae1c60ab4a3c5
MatteoGaloni <mtgaloni@gmail.com>
2023-05-23 16:19:55 +0200

    add cards_wrapper and slide element

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,7 +8,50 @@ export default {
 </script>
 
 <template>
-    <h2>MAIN</h2>
+    <main>
+        <div id="cards_frame" class="container">
+            <p>FOUND 50 CARDS</p>
+            <div id="cards_wrapper">
+                <div class="slide">
+                    <img class="thumb" src="../assets/vue.svg" alt="#">
+                    <p>Descrizione</p>
+                    <h5>TITOLO CARD</h5>
+                </div>
+                <div class="slide">
+                    <img class="thumb" src="../assets/vue.svg" alt="#">
+                    <p>Descrizione</p>
+                    <h5>TITOLO CARD</h5>
+                </div>
+
+
+
+            </div>
+        </div>
+
+    </main>
 </template> 
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+main {
+    background-color: orange;
+
+    #cards_frame {
+        min-height: 600px;
+        background-color: white;
+
+    }
+
+    #cards_wrapper {
+        min-height: 300px;
+        background-color: pink;
+        display: flex;
+        flex-wrap: wrap;
+    }
+}
+
+.slide {
+    width: calc(100% /5);
+    padding: 1rem;
+    background-color: orange;
+}
+</style>
