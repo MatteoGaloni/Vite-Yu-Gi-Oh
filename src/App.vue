3b59abfe4464952901004e35e913ed6a7c3f041a
MatteoGaloni <mtgaloni@gmail.com>
2023-05-23 15:25:48 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="prova">
+    <h1>YU GI OH</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.prova {
+  background-color: red;
+
+  & :hover {
+    background-color: orange;
+  }
+}
+</style>
